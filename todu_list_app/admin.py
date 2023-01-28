48c83a18eb19cb6e2fd0b52a6254af628ed3fbb4
Og'abek <ogabek@pop-os.localdomain>
2023-01-28 18:12:12 +0500

    craete models in todu list

--- todu_list_app/admin.py
+++ todu_list_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Todu
 
 # Register your models here.
+admin.site.register(Todu)
+admin.site.register(User)
--- todu_list_app/models.py
+++ todu_list_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.TextField(max_length=20)
+    email = models.EmailField()
+    password = models.CharField(max_length=12)
+
+    def __str__(self) -> str:
+        return self.username
+
+class Todu(models.Model):
+    user = models.ForeignKey(User, models.Case, related_name='todu')
+    title = models.CharField(max_length=200)
+    discreption = models.CharField(max_length=50)
+    chekt = models.BooleanField(blank=False)
